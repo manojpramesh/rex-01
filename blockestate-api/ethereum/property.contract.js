3e91dd46e583e65f2fc88c86e2f8b2f96e2e5136
Manoj P R <manojpramesh@gmail.com>
2017-10-25 23:16:07 +0530

    contract helpers

---
+++ blockestate-api/ethereum/property.contract.js
@@ -0,0 +1,39 @@
+const ethereumHelper = require('../helpers/ethereum.contract.helper');
+const config = require('../config/ethereum');
+
+module.exports = {
+
+    getProperty: (params) => {
+        return ethereumHelper.readContract(params, config.RealEstate.address, config.RealEstate.abi, 'properties');
+    },
+
+    addProperty: (params) => {
+        return ethereumHelper.writeContract(config.accounts[0].address,
+            config.accounts[0].privateKey,
+            0,
+            config.RealEstate.address,
+            config.RealEstate.abi,
+            'addProperty',
+            params);
+    },
+
+    editProperty: (params) => {
+        return ethereumHelper.writeContract(config.accounts[0].address,
+            config.accounts[0].privateKey,
+            0,
+            config.RealEstate.address,
+            config.RealEstate.abi,
+            'editProperty',
+            params);
+    },
+
+    deleteProperty: (params) => {
+        return ethereumHelper.writeContract(config.accounts[0].address,
+            config.accounts[0].privateKey,
+            0,
+            config.RealEstate.address,
+            config.RealEstate.abi,
+            'deleteProperty',
+            params)
+    }
+}
