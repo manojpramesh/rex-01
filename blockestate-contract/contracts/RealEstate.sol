0a6f9643f0463e8aaa129ed5332f54d48def8c1b
Manoj P R <manojpramesh@gmail.com>
2017-10-25 23:11:43 +0530

    Best practices

--- blockestate-contract/contracts/RealEstate.sol
+++ blockestate-contract/contracts/RealEstate.sol
@@ -1,24 +1,52 @@
 pragma solidity^0.4.13;
 
+/// @title Contract to manage real estate dealings in blockchain
 contract RealEstate {
     
+    // declares owner of the contract
     address _owner;
-    int public _length;
-    mapping (int => string) properties;
     
+    // declares a state varible that stores property details.
+    mapping (int => string) public properties;
+    
+    // modifier to restrict contract usage
+    modifier onlyOwner() {
+        require(msg.sender == _owner);
+        _;
+    }
+    
+    // constructor that saves owner details.
     function RealEstate() public {
         _owner = msg.sender;
     }
     
-    function addProperty(int _id, string _data) public {
+    // function to add property details
+    function addProperty(int _id, string _data) onlyOwner public {
         properties[_id] = _data;
     }
     
-    function editProperty(int _id, string _data) public {
+    // function to edit property details
+    function editProperty(int _id, string _data) onlyOwner public {
         properties[_id] = _data;
     }
     
-    function deleteProperty(int _id) public {
+    // function to delete a certain property
+    function deleteProperty(int _id) onlyOwner public {
         properties[_id] = "";
+    }
+    
+    // change the owner of the contract
+    function changeOwner(address newOwner) onlyOwner public {
+        _owner = newOwner;
+    }
+    
+    // destroy the current contract, sending its funds to the given address
+    function kill() onlyOwner public { 
+        selfdestruct(_owner);
+    }
+    
+    // fallback function to prevent sending ethers to the contract
+    function () payable public {
+        revert();
     }
 }
